f87d6cbf0992eb6e6cb5879fa28013bc089f4381
J-Alexandr <lprankerl@gmail.com>
2016-11-17 22:08:31 +0200

    SourceProvider interface implemented

---
+++ lesson5/src/task1/source/FileSourceProvider.java
@@ -0,0 +1,30 @@
+package task1.source;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Implementation for loading content from local file system.
+ * This implementation supports absolute paths to local file system without specifying file:// protocol.
+ * Examples c:/1.txt or d:/pathToFile/file.txt
+ */
+public class FileSourceProvider implements SourceProvider {
+
+    @Override
+    public boolean isAllowed(String pathToSource) {
+        return new File(pathToSource).isFile();
+    }
+
+    @Override
+    public String load(String pathToSource) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(pathToSource));
+        StringBuilder stringBuilder = new StringBuilder();
+        while (reader.ready()) {
+            stringBuilder.append(reader.readLine());
+            if (reader.ready()) stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
