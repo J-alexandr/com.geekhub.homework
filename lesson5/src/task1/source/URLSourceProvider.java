66390e542523a152556a326828f561aac34d9c0d
J-Alexandr <lprankerl@gmail.com>
2016-11-17 22:39:27 +0200

    SourceProvider interface implemented

--- lesson5/src/task1/source/URLSourceProvider.java
+++ lesson5/src/task1/source/URLSourceProvider.java
@@ -1,6 +1,10 @@
 package task1.source;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Implementation for loading content from specified URL.<br/>
@@ -10,13 +14,28 @@ public class URLSourceProvider implements SourceProvider {
 
     @Override
     public boolean isAllowed(String pathToSource) {
-        //TODO: implement me
-        return false;
+        try {
+            URL url = new URL(pathToSource);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public String load(String pathToSource) throws IOException {
-        //TODO: implement me
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        URL url = new URL(pathToSource);
+        URLConnection connection = url.openConnection();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            while (reader.ready()) {
+                stringBuilder.append(reader.readLine());
+                if (reader.ready()) stringBuilder.append("\n");
+            }
+        }
+        return stringBuilder.toString();
     }
 }
