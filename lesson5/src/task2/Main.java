144efc3324063019109486ab77168f8a8ef86bab
lprankerl@gmail.com <Gordons10death>
2016-11-19 15:04:49 +0200

    Init commit

---
+++ lesson5/src/task2/Main.java
@@ -0,0 +1,21 @@
+package task2;
+
+/*
+ Zip File Maker
+
+ The user enters a path to a directory and the program zips up the directory items into several zip files.
+ The program should zip up files of the given directory and all the sub-directories.
+ The program supports such types:
+ - audio (mp3, wav, wm);
+ - video (avi, mp4, flv);
+ - image (jpeg, jpg, gif, tiff, png);
+ As the result you must have 3 archives: audio.zip, video.zip and image.zip.
+ You can use File, FileFilter, (G)ZIPOutputStream or something else.
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
